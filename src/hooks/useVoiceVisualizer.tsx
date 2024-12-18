ec0477210198440998d4fb3d4392460346885470
Neil Fisher <neil3524@gmail.com>
2024-12-18 12:35:32 -0500

    Remove unnecessary check for recording status in stopRecording function

--- src/hooks/useVoiceVisualizer.tsx
+++ src/hooks/useVoiceVisualizer.tsx
@@ -217,8 +217,6 @@ function useVoiceVisualizer({
   };
 
   const stopRecording = () => {
-    if (!isRecordingInProgress) return;
-
     setIsRecordingInProgress(false);
     if (mediaRecorderRef.current) {
       mediaRecorderRef.current.stop();
